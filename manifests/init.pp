004584c28abe9f1c5c2cbe03fe48837bd9c7b39c
Greg Kitson <greg.kitson@gmail.com>
2014-08-29 15:02:46 -0700

    add first version of nimbus module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# installs nimbus, the irccloud app
+class nimbus (
+  $version = '0.7.3',
+) {
+  package { 'Nimbus':
+    ensure   => 'present',
+    source   => "https://github.com/jnordberg/irccloudapp/releases/download/${version}/Nimbus-${version}.zip",
+    provider => 'compressed_app',
+    flavor   => 'zip',
+  }
 }
