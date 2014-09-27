b19e837641c125070ab25f8376e6de4a657d33a3
Greg Kitson <greg.kitson@gmail.com>
2014-09-27 11:58:12 -0700

    retab nimbus_spec.rb

--- spec/classes/nimbus_spec.rb
+++ spec/classes/nimbus_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 describe 'nimbus' do
   it do
-		should contain_package('Nimbus').with({
-			:ensure   => 'present',
-			:source   => 'https://github.com/jnordberg/irccloudapp/releases/download/0.7.3/Nimbus-0.7.3.zip',
-			:provider => 'compressed_app',
-			:flavor   => 'zip',
-		})
+    should contain_package('Nimbus').with({
+      :ensure   => 'present',
+      :source   => 'https://github.com/jnordberg/irccloudapp/releases/download/0.7.3/Nimbus-0.7.3.zip',
+      :provider => 'compressed_app',
+      :flavor   => 'zip',
+    })
   end
 end
